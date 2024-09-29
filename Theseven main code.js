711d4dc0814581c855ec5443f8bdf6129ce1b22c
Ragna <waynepfusire@gmail.com>
2024-09-29 23:54:46 +0100

    Update Theseven main code.js

--- Theseven main code.js
+++ Theseven main code.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log('Bot is online!');
+});
+
+client.on('message', (message) => {
+  if (message.content.startsWith('!bye')) {
+    message.channel.send('Goodbye!');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
